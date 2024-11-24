2cb82a7773a5c1936bf56d7300c2892f33bc01e5
Elreek <sorczoy@gmail.com>
2024-11-24 12:48:58 +0000

    sobre mi añadido

--- src/components/Aboutme.vue
+++ src/components/Aboutme.vue
@@ -1,15 +1,50 @@
 <template>
-  <section id="about">
-    <h2>{{ $t("aboutme") }}</h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut enim sed
-      justo ultricies elementum.
-    </p>
-  </section>
+  <div class="aboutme container py-5">
+    <div class="d-flex flex-column row align-items-center">
+      <!-- Imagen de perfil -->
+      <div class="col-md-4 text-center mb-5">
+        <img
+          src="../assets/veinteañitostengo.jpg"
+          alt="Foto de perfil"
+          class="img-fluid rounded-circle shadow"
+        />
+      </div>
+
+      <!-- Información personal -->
+      <div class="col-md-8">
+        <h1 class="display-4 mb-5">{{ $t("aboutme") }}</h1>
+        <p class="lead">
+          {{ $t("description") }}
+        </p>
+        <ul class="list-unstyled">
+          <li>
+            <b><b-icon-person /> {{ $t("name") }}:</b> {{ $t("name_value") }}
+          </li>
+          <li>
+            <b><b-icon-envelope-at /> {{ $t("email") }}:</b> 
+            <a href="mailto:elreekdev33@gmail.com"> elreekdev33@gmail.com</a>
+          </li>
+          <li>
+            <b><b-icon-geo-alt /> {{ $t("location") }}:</b> {{ $t("location_value") }}
+          </li>
+        </ul>
+
+        <h2 class="mt-4">{{ $t("interests_title") }}</h2>
+        <p>{{ $t("interests_description") }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+</script>
 
 <style scoped>
-/* Estilos específicos */
+.aboutme img {
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+}
 </style>
