81d504693f6df1c31d0c262c6076cade18b35376
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-11-24 17:58:37 +0000

    Se avanza apartado 6 y 7

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,5 +1,14 @@
+<script setup>
+    import { useI18n } from "vue-i18n";
+  
+    const { locale, t } = useI18n();
+</script>
+
 <template>
     <section id="projects">
         <h1>Proyectos</h1>
+        <div class="row row-cols-3">
+            
+        </div>
     </section>
 </template>
