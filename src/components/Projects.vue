6a005150ab55aa4af22459d7773fc2f5bf9c4701
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-11-26 02:25:10 +0000

    Se adelantan apartados 6 y 7

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,14 +1,16 @@
 <script setup>
     import { useI18n } from "vue-i18n";
-  
-    const { locale, t } = useI18n();
+    const locale = useI18n();
+    const currentLocale = locale.getLocaleMessage(locale.locale.value);
+    
 </script>
 
 <template>
     <section id="projects">
         <h1>Proyectos</h1>
         <div class="row row-cols-3">
-            
+            {{ currentLocale["project-cards"] }}
+            <hr>
         </div>
     </section>
 </template>
